c59d976a94082edde6f9cb3750fa7bac27b76b85
NoahFrahm <nofrahm@live.unc.edu>
2022-09-29 16:42:32 -0400

    fixed arg parsing
    properly parsed user args

--- cli.js
+++ cli.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 import minimist from "minimist";
-import moment from "moment-timezone";
+// import moment from "moment-timezone";
 
 // const timezone = moment.tz.guest();
+
+//run from command line using node cli.js
 
 function commandy(x){
     let m = "Hello, " + x + "."
     return m
 }
 
-const message_in = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2))
+const message_in = args.m
 const message_out = commandy(message_in)
 
 console.log(message_out)
