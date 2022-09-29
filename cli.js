c7bfec54beb0db975181d3379d2ae55c3a56d618
NoahFrahm <nofrahm@live.unc.edu>
2022-09-29 16:32:53 -0400

    process user args
    added minimist and test function to see if command line args are taken in

--- cli.js
+++ cli.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import minimist from "minimist";
+import moment from "moment-timezone";
+
+// const timezone = moment.tz.guest();
+
+function commandy(x){
+    let m = "Hello, " + x + "."
+    return m
+}
+
+const message_in = minimist(process.argv.slice(2))
+const message_out = commandy(message_in)
+
+console.log(message_out)
+
+// const response = await fetch('');
+// const data = await response.data()
